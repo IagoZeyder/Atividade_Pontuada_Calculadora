82226aaed32ca7f1a567bd61f65f517709ea03d8
IagoZeyder <iagothebano2001@gmail.com>
2021-11-06 11:52:51 -0300

    Atividade_Pontuada_Calculadora
    Atividade_Pontuada_Calculadora

---
+++ Atividade_Pontuada_Calculadora/src/atividade_pontuada_calculadora_java_desenvolvimento_de_sistemas/Atividade_Pontuada_Calculadora_Java_Desenvolvimento_de_Sistemas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade_pontuada_calculadora_java_desenvolvimento_de_sistemas;
+
+/**
+ *
+ * @author paren
+ */
+public class Atividade_Pontuada_Calculadora_Java_Desenvolvimento_de_Sistemas {
+    public static void main(String[] args) {
+      System.out.println("Atividade Pontuada Java, Desenvolvimento de Sistemas");
+    }
+    
+}
